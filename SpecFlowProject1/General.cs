024b2f9337d50b8430f2a1cb69deddf68aeb1314
VSonV <34054089+VSonV@users.noreply.github.com>
2022-05-20 11:32:44 +0700

    Initial commit

---
+++ SpecFlowProject1/General.cs
@@ -0,0 +1,25 @@
+﻿namespace TestingProject
+{
+    private static IConfigurationRoot GetConfigurationRoot()
+    {
+        IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json"));
+        return configurationBuilder.Build();
+    }
+
+    public enum RunEnvironment : sbyte
+    {
+        Local = 1,
+        Staging,
+        Production
+    }
+
+    public enum Browser : sbyte
+    {
+        Chrome = 1,
+        FireFox,
+        Edge,
+        IE,
+        Opera
+    }
+}
