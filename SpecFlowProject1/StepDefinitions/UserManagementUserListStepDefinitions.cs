298e0df9257442cfa514662d8e0c22348027ca81
VSonV <34054089+VSonV@users.noreply.github.com>
2022-05-25 11:29:48 +0700

    update verify sort function

--- SpecFlowProject1/StepDefinitions/UserManagementUserListStepDefinitions.cs
+++ SpecFlowProject1/StepDefinitions/UserManagementUserListStepDefinitions.cs
@@ -5,6 +5,7 @@ using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 using TechTalk.SpecFlow;
 using OpenQA.Selenium.Interactions;
+using NUnit.Framework;
 
 namespace TestingProject.StepDefinitions
 {
@@ -17,6 +18,17 @@ namespace TestingProject.StepDefinitions
         private string _Pwd;
         private IConfigurationRoot _configuration;
         private int _delayTime;
+
+        StringBuilder userTypeBefore;
+        StringBuilder firstNameBefore;
+        StringBuilder lastNameBefore;
+        StringBuilder userNameBefore;
+        StringBuilder emailNameBefore;
+        StringBuilder userTypeAfter;
+        StringBuilder firstNameAfter;
+        StringBuilder lastNameAfter;
+        StringBuilder userNameAfter;
+        StringBuilder emailNameAfter;
 
         [Given(@"The user launches the application")]
         public void GivenTheUserLaunchesTheApplication()
@@ -101,7 +113,7 @@ namespace TestingProject.StepDefinitions
         [Then(@"The user sees elements on User list as the following table")]
         public void ThenTheUserSeesElementsOnUserListAsTheFollowingTable()
         {
-
+            Thread.Sleep(_delayTime);
             var numberPerPageFilter = _driver.FindElement(By.Id("vs1__combobox"))?.Text;//number per page filter
             var userTypeFilter = _driver.FindElement(By.Id("vs4__combobox"))?.Text;//user type filter
             var statusFilter = _driver.FindElement(By.Id("vs5__combobox"))?.Text;//status filter
@@ -119,46 +131,110 @@ namespace TestingProject.StepDefinitions
 
             var paging = _driver.FindElements(By.ClassName("pagination-wrapper"));//paging
 
+            _driver.Quit();
+
         }
 
         [When(@"The user clicks on <column name> on User List")]
         public void WhenTheUserClicksOnColumnNameOnUserList()
         {
-            //Thread.Sleep(_delayTime);
-            //_driver.FindElement(By.XPath("//div[text()=' User Type ']"))?.Click();//user type column
-            
-            //Thread.Sleep(_delayTime);
-            //_driver.FindElement(By.XPath("//div[text()=' First Name ']"))?.Click();//first name column
+            //get user type value before and after click event
+            Thread.Sleep(_delayTime);
+            userTypeBefore = new StringBuilder();
+            GetTableVal(ref userTypeBefore,0);
+            _driver.FindElement(By.XPath("//div[text()=' User Type ']"))?.Click();//user type column
+            Thread.Sleep(_delayTime);
+            userTypeAfter = new StringBuilder();
+            GetTableVal(ref userTypeAfter, 0);
 
-            //Thread.Sleep(_delayTime);
-            //_driver.FindElement(By.XPath("//div[text()=' Last Name ']"))?.Click();//last name column
-
-            //Thread.Sleep(_delayTime);
-            //_driver.FindElement(By.XPath("//div[text()=' Username ']"))?.Click();//user name column
+            //get first name value before and after click event
+            Thread.Sleep(_delayTime);
+            firstNameBefore = new StringBuilder();
+            GetTableVal(ref firstNameBefore, 1);
+            _driver.FindElement(By.XPath("//div[text()=' First Name ']"))?.Click();//first name column
+            Thread.Sleep(_delayTime);
+            firstNameAfter = new StringBuilder();
+            GetTableVal(ref firstNameAfter, 1);
 
             Thread.Sleep(_delayTime);
-            _driver.FindElement(By.XPath("//div[text()=' Email ']"))?.Click();//email column
+            lastNameBefore = new StringBuilder();
+            GetTableVal(ref lastNameBefore, 2);
+            _driver.FindElement(By.XPath("//div[text()=' Last Name ']"))?.Click();//last name column
+            Thread.Sleep(_delayTime);
+            lastNameAfter = new StringBuilder();
+            GetTableVal(ref lastNameAfter, 2);
 
-            
+            Thread.Sleep(_delayTime);
+            userNameBefore = new StringBuilder();
+            GetTableVal(ref userNameBefore, 3);
+            _driver.FindElement(By.XPath("//div[text()=' Username ']"))?.Click();//user name column
+            Thread.Sleep(_delayTime);
+            userNameAfter = new StringBuilder();
+            GetTableVal(ref userNameAfter, 3);
+
+            Thread.Sleep(_delayTime);
+            emailNameBefore = new StringBuilder();
+            GetTableVal(ref emailNameBefore, 4);
+            _driver.FindElement(By.XPath("//div[text()=' Email ']"))?.Click();//email column
+            Thread.Sleep(_delayTime);
+            emailNameAfter = new StringBuilder();
+            GetTableVal(ref emailNameAfter, 4);
         }
 
         [Then(@"the <column name> is sorted on User List")]
         public void ThenTheColumnNameIsSortedOnUserList()
         {
-            var userTypeList = new StringBuilder();
-            //get table rows
+            var userTypeValBefore = userTypeBefore.ToString();
+            var userTypeValAfter = userTypeAfter.ToString();
+            Assert.IsFalse(userTypeValBefore.Equals(userTypeValAfter));
+
+            var firstNameValBefore = firstNameBefore.ToString();
+            var firstNameValAfter = firstNameAfter.ToString();
+            Assert.IsFalse(firstNameValBefore.Equals(firstNameValAfter));
+
+            var lastNameValBefore = lastNameBefore.ToString();
+            var lastNameValAfter = lastNameAfter.ToString();
+            Assert.IsFalse(lastNameValBefore.Equals(lastNameValAfter));
+
+            var uNameValBefore = userNameBefore.ToString();
+            var uNameValAfter = userNameAfter.ToString();
+            Assert.IsFalse(uNameValBefore.Equals(uNameValAfter));
+
+            var emailValBefore = emailNameBefore.ToString();
+            var emailValAfter = emailNameAfter.ToString();
+            Assert.IsFalse(emailValBefore.Equals(emailValAfter));
+
+            userTypeBefore.Clear();
+            userTypeAfter.Clear();
+            firstNameBefore.Clear();
+            firstNameAfter.Clear();
+            lastNameBefore.Clear();
+            lastNameAfter.Clear();
+            userNameBefore.Clear();
+            userNameAfter.Clear();
+            emailNameBefore.Clear();
+            emailNameAfter.Clear();
+
+            _driver.Quit();
+        }
+
+        private void GetTableVal(ref StringBuilder fieldVal, int colPosition)
+        {
             var userTable = _driver.FindElements(By.XPath("//div[@class='table-fix-header']/tbody/tr"));
-            foreach(var tblRow in userTable)
+            foreach (var tblRow in userTable)
             {
                 var columns = tblRow.Text.Split("\r");
                 for (int i = 0; i < columns.Length; i++)
                 {
-                    userTypeList.Append(i);
+                    if (i == colPosition)
+                    {
+                        fieldVal.Append(columns[i]);
+                        break;
+                    }
                 }
             }
-            var tmp = userTypeList.ToString();
-        }
 
+        }
     }
 }
 
